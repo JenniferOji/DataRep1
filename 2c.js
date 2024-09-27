286ea778d3957da9f1dc6b8a9ae7a1de0653c01e
JenniferOji <g00413791@atu.ie>
2024-09-27 08:50:15 +0000

    2a finished

---
+++ 2c.js
@@ -0,0 +1,8 @@
+//the method recieving the passed parameters 
+const x = (value1, value2)=>{
+    //adding the two values in the function together 
+    console.log(value1 + value2);
+}
+
+//the values im passing to the function
+x(10,5);
